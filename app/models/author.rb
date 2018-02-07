08763b97d431fadbc98355a82e28e48dbbbab244
Min Kim <mk4555@nyu.edu>
2018-02-07 21:52:20 +0000

    automatically backed up by learn

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,5 @@
 class Author < ActiveRecord::Base
   validates :name, presence: true
   validates :phone_number, length: {minimum: 10}
-  validates :name, uniqueness: true
+  validates :email, uniqueness: true
 end
