f52c3a741ab009a0e95ae480e65bc12e767ae877
Min Kim <mk4555@nyu.edu>
2018-02-07 21:31:37 +0000

    automatically backed up by learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -9,8 +9,8 @@ class AuthorsController < ApplicationController
 
   def create
     @author = Author.new(author_params)
-    @author = Author.create(author_params)
-
+    if @author.valid?
+      @author.save
     redirect_to author_path(@author)
   end
 
