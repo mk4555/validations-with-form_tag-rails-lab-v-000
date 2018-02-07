5995f22edc402cdd7765559f759957aaf3f58c68
Min Kim <mk4555@nyu.edu>
2018-02-07 21:51:29 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :authors, only: [:index, :show, :new, :create, :edit, :update]
-  resources :posts, only: [:index, :show, :new, :create, :edit, :update]
+  resources :authors
+  resources :posts,
 end
